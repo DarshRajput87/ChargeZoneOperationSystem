d799374ca5afe2ba548206686667619a8725e2d1
DarshRajput87 <darshrajput87@gmail.com>
2026-03-20 12:21:02 +0530

    production update

--- frontend/src/services/fleetService.js
+++ frontend/src/services/fleetService.js
@@ -5,8 +5,11 @@ const API = "https://api.chargezoneops.online/api";
 export const getTenants = () =>
     axios.get(`${API}/fleet/tenants`);
 
-export const getOcpiClients = () =>
-    axios.get(`${API}/fleet/ocpi-clients`);
+export const getOcpiClients = (tenant_id) =>
+    axios.get(`${API}/fleet/ocpi-clients`, { params: tenant_id ? { tenant_id } : {} });
+
+export const getExistingFleets = (tenant_id) =>
+    axios.get(`${API}/fleet/existing-fleets`, { params: { tenant_id } });
 
 export const createFleet = (data) =>
     axios.post(`${API}/fleet/create`, data);
